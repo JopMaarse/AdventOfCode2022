dff39cf36da59fc6a8706e4f766c357366549454
JopMaarse <jop_maarse@hotmail.com>
2022-12-01 18:56:04 +0100

    Day 1

---
+++ AdventOfCode2022/Day1/Day1.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2022.Day1;
+
+internal class Day1
+{
+    public static async Task<string> Solve(Stream stream)
+    {
+        using StreamReader streamReader= new(stream);
+        string input = await streamReader.ReadToEndAsync();
+        IEnumerable<int> calories = 
+            input
+                .Split("\n\n")
+                .Select(elf => elf
+                    .Split("\n")
+                    .Where(s => !string.IsNullOrEmpty(s))
+                    .Select(int.Parse)
+                    .Sum());
+
+        string part1 =
+            calories
+                .Max()
+                .ToString();
+                
+        return calories
+            .OrderDescending()
+            .Take(3)
+            .Sum()
+            .ToString();            
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode2022.Day1;
+const string fileName = "input.txt";
+using Stream input = File.Open(@$"{nameof(Day1)}\{fileName}", FileMode.Open);
+Console.WriteLine(await Day1.Solve(input));
