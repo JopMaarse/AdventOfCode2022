920b553048146637d36332856af5a71e2725c79d
JopMaarse <jop_maarse@hotmail.com>
2022-12-02 16:13:22 +0100

    Day 2

---
+++ AdventOfCode2022/Day2/Day2.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode2022.Day2;
+
+internal class Day2
+{
+    public static async Task<string> Solve(Stream stream)
+    {
+        using StreamReader streamReader = new(stream);
+        int score = 0;
+        string? input;
+        while((input = await streamReader.ReadLineAsync()) is not null)
+        {
+            score += ScorePart2(input);
+        }
+        return score.ToString();
+    }
+
+    private static int ScorePart1(string round) => round switch
+    {
+        "A X" => 4,
+        "A Y" => 8,
+        "A Z" => 3,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 7,
+        "C Y" => 2,
+        "C Z" => 6
+    };
+    
+    private static int ScorePart2(string round) => round switch
+    {
+        "A X" => 3,
+        "A Y" => 4,
+        "A Z" => 8,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 2,
+        "C Y" => 6,
+        "C Z" => 7
+    };
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,4 @@
-﻿using AdventOfCode2022.Day1;
+﻿using AdventOfCode2022.Day2;
 const string fileName = "input.txt";
-using Stream input = File.Open(@$"{nameof(Day1)}\{fileName}", FileMode.Open);
-Console.WriteLine(await Day1.Solve(input));
+using Stream input = File.Open(@$"{nameof(Day2)}\{fileName}", FileMode.Open);
+Console.WriteLine(await Day2.Solve(input));
