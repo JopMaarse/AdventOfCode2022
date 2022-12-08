306bd896f0b954d5b989b5e59c9ac149a1fff4fe
JopMaarse <jop_maarse@hotmail.com>
2022-12-08 17:40:34 +0100

    Day8 part 2

--- AdventOfCode2022/Day8/Day8.cs
+++ AdventOfCode2022/Day8/Day8.cs
@@ -1,8 +1,86 @@
-﻿namespace AdventOfCode2022.Day8;
+﻿using System.IO;
+
+namespace AdventOfCode2022.Day8;
 
 internal class Day8
 {
     public static async Task<string> Solve(Stream stream)
+    {
+        int[,] grid = await ParseInput(stream);
+        int visibleTrees = 0;
+        int bestScenicScore = 0;
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                //edge
+                if (i == 0 || j == 0 || i == grid.GetLength(0) - 1 || j == grid.GetLength(1) - 1)
+                {
+                    visibleTrees++;                    
+                    continue;
+                }
+
+                int scenicScore = 1;
+
+                bool visibleRight = true;
+                int k;
+                for (k = i + 1; k < grid.GetLength(0); k++)
+                {
+                    if (grid[k, j] >= grid[i, j])
+                    {
+                        visibleRight = false;
+                        break;
+                    }
+                }
+                scenicScore *= k == grid.GetLength(0) ? k - 1 - i : k - i;
+
+                bool visibleLeft = true;
+                for (k = i - 1; k >= 0; k--)
+                {
+                    if (grid[k, j] >= grid[i, j])
+                    {
+                        visibleLeft = false;
+                        break;
+                    }
+                }
+                scenicScore *= k == -1 ? i : i - k;
+
+                bool visibleDown = true;
+                for (k = j + 1; k < grid.GetLength(1); k++)
+                {
+                    if (grid[i, k] >= grid[i, j])
+                    {
+                        visibleDown = false;
+                        break;
+                    }
+                }
+                scenicScore *= k == grid.GetLength(1) ? k - 1 - j : k - j;
+
+                bool visibleUp = true;
+                for (k = j - 1; k >= 0; k--)
+                {
+                    if (grid[i, k] >= grid[i, j])
+                    {
+                        visibleUp = false;
+                        break;
+                    }
+                }
+                scenicScore *= k == -1 ? j : j - k;
+
+                if (visibleRight || visibleLeft || visibleDown || visibleUp)
+                {
+                    visibleTrees++;
+                }
+
+                if (scenicScore > bestScenicScore)
+                    bestScenicScore = scenicScore;
+            }
+        }
+
+        return $"{visibleTrees}{Environment.NewLine}{bestScenicScore}";
+    }
+
+    private static async Task<int[,]> ParseInput(Stream stream)
     {
         using StreamReader streamReader = new(stream);
         string[] lines = (await streamReader.ReadToEndAsync()).Split('\n', StringSplitOptions.RemoveEmptyEntries);
@@ -15,88 +93,7 @@ internal class Day8
             }
         }
 
-        int visibleTrees = 0;
-        for (int i = 0; i < grid.GetLength(0); i++)
-        {
-            for (int j = 0; j < grid.GetLength(1); j++)
-            {
-                //edge
-                if (i == 0 || j == 0 || i == grid.GetLength(0) - 1 || j == grid.GetLength(1) - 1)
-                {
-                    visibleTrees++;
-                    continue;
-                }
-
-                //look right
-                bool visible = true;
-                for (int k = i + 1; k < grid.GetLength(0); k++)
-                {
-                    if (grid[k, j] >= grid[i, j])
-                    {
-                        visible = false;
-                        break;
-                    }
-                }
-
-                if (visible)
-                {
-                    visibleTrees++;
-                    continue;
-                }
-
-                //look left
-                visible = true;
-                for (int k = i - 1; k >= 0; k--)
-                {
-                    if (grid[k, j] >= grid[i, j])
-                    {
-                        visible = false;
-                        break;
-                    }
-                }
-
-                if (visible)
-                {
-                    visibleTrees++;
-                    continue;
-                }
-
-                //look down
-                visible = true;
-                for (int k = j + 1; k < grid.GetLength(1); k++)
-                {
-                    if (grid[i, k] >= grid[i, j])
-                    {
-                        visible = false;
-                        break;
-                    }
-                }
-
-                if (visible)
-                {
-                    visibleTrees++;
-                    continue;
-                }
-
-                //look up
-                visible = true;
-                for (int k = j - 1; k >= 0; k--)
-                {
-                    if (grid[i, k] >= grid[i, j])
-                    {
-                        visible = false;
-                        break;
-                    }
-                }
-
-                if (visible)
-                {
-                    visibleTrees++;
-                }
-            }
-        }
-
-        return visibleTrees.ToString();
+        return grid;
     }
 }
 
